c6b745e91195636b5e391a9168090075b1dd4f90
kazuki-77 <93814427+kazuki-77@users.noreply.github.com>
2021-11-29 09:19:40 +0000

    [finish] 3-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'eaf6cfc8252ba54a2c13aa7a787e353c9550f27dc16174d1be9d9754d5c180099ba33d87eb4959bc626fc8a792c823b6a5f195ba90b1420bf2960eee2d365a5c'
