cbdd73dd0d9d67680556b3584549cf9183959d33
kazuki-77 <93814427+kazuki-77@users.noreply.github.com>
2021-11-29 06:05:54 +0000

    [finish] 3-4

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_29_043831) do
+ActiveRecord::Schema.define(version: 2021_11_29_043950) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
   end
 
 end
